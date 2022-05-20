869a1ab04616dffebd94e42620ea64c72356a414
ubuntu <tagir-net@mail.ru>
2022-05-20 17:14:29 +0530

    Docker commit

--- djangoTeamForce/settings.py
+++ djangoTeamForce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-@0(vxg4f6&^2u38uvjvi5mrn=z4xc23-pqv@sge_lm!t8fl#c8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
